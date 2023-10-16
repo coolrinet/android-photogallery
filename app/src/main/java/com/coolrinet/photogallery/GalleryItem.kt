19b17569029eba9ddb8b21bd5df95eba39917b8b
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2023-10-16 14:27:01 +0700

    changed GET request to the Flickr API and created GalleryItem

---
+++ app/src/main/java/com/coolrinet/photogallery/GalleryItem.kt
@@ -0,0 +1,7 @@
+package com.coolrinet.photogallery
+
+data class GalleryItem(
+    val title: String,
+    val id: String,
+    val url: String
+)
--- app/src/main/java/com/coolrinet/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/coolrinet/photogallery/api/FlickrApi.kt
@@ -2,7 +2,15 @@ package com.coolrinet.photogallery.api
 
 import retrofit2.http.GET
 
+private const val API_KEY = "a31717e733d55b0a10a0bd2a17ab5682"
+
 interface FlickrApi {
-    @GET("/")
-    suspend fun fetchContents(): String
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=$API_KEY" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    suspend fun fetchPhotos(): String
 }
